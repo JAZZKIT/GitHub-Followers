416173bbe579156e6962032fd1afda4557197ee3
Denny McYuri <nordicwolf.d@gmail.com>
2022-01-04 17:56:43 +0200

    Base Setup was created

--- GitHub Followers/FavoritesListVC.swift
+++ GitHub Followers/FavoritesListVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  FavoritesListVC.swift
 //  GitHub Followers
 //
 //  Created by Denny on 04.01.2022.
@@ -7,13 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FavoritesListVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemRed
     }
-
-
 }
-
---
+++ GitHub Followers/SearchVC.swift
@@ -0,0 +1,17 @@
+//
+//  SearchVC.swift
+//  GitHub Followers
+//
+//  Created by Denny on 04.01.2022.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+   
+}
