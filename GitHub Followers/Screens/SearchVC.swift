166e0a93e8466413880b529b6cc7bde529a366c8
Denny McYuri <nordicwolf.d@gmail.com>
2022-01-05 21:59:26 +0200

    Custom alert and NetworkManager were created

--- GitHub Followers/Screens/SearchVC.swift
+++ GitHub Followers/Screens/SearchVC.swift
@@ -26,7 +26,7 @@ class SearchVC: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        navigationController?.isNavigationBarHidden = true
+        navigationController?.setNavigationBarHidden(true, animated: true)
     }
     
     func createDismissKeyboardTapGesture() {
@@ -36,7 +36,7 @@ class SearchVC: UIViewController {
     
     @objc func pushFollowerListVC() {
         guard isUsernameEntered else {
-            print("No user")
+            presentGFAlertOnMainThread(title: "Empty Username", message: "Please enter a username. We need to know who to look for 😇", buttonTitle: "OK")
             return
         }
         
